bb9a3742d22f44b7fb35a3fdf55d83eea1b7287c
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-24 17:18:39 -0300

    Creando el router de presupuestos

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
 import {db} from './config/db'
+import budgetRouter from './routes/budgetRouter'
 
 async function connectDB() {
     try {
@@ -18,5 +19,7 @@ const app = express()
 app.use(morgan('dev'))
 
 app.use(express.json())
+
+app.use('/api/budgets', budgetRouter)
 
 export default app
