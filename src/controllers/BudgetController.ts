33deb9c4568c9e00bd2132e3df4680cda2f43685
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-24 17:50:52 -0300

    Agregando todas las rutas y métodos del controlador

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -8,4 +8,22 @@ export class BudgetController {
     static create = async  (req: Request, res: Response) => {
         console.log('oliwis post')
     }
+
+    static getById = async  (req: Request, res: Response) => {
+        console.log('oliwis id')
+        const {id} = req.params
+        console.log(id)
+    }
+
+    static updateById = async  (req: Request, res: Response) => {
+        console.log('oliwis id put')
+        const {id} = req.params
+        console.log(id)
+    }
+
+    static deleteById = async  (req: Request, res: Response) => {
+        console.log('oliwis id delete')
+        const {id} = req.params
+        console.log(id)
+    }
 }
