b1b5e361c3fd53fa16f74fa4acd756b5735e9b67
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-25 15:29:13 -0300

    Creación de presupuestos

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,28 +1,37 @@
 import type {Request, Response} from 'express'
+import Budget from '../models/Budget'
 
 export class BudgetController {
-    static getAll = async  (req: Request, res: Response) => {
-        console.log('oliwis')
+    static getAll = async (req: Request, res: Response) => {
+        console.log('oliwis getall get')
     }
 
-    static create = async  (req: Request, res: Response) => {
-        console.log('oliwis post')
+    static create = async (req: Request, res: Response) => {
+        console.log('oliwis create post')
+        try {
+            const budget = new Budget(req.body)
+            await budget.save()
+            res.status(200).json('Presupuesto creado correctamente')
+        } catch (error) {
+            // console.log(error)
+            res.status(500).json({error: 'Hubo un errorsinho'})
+        }
     }
 
-    static getById = async  (req: Request, res: Response) => {
-        console.log('oliwis id')
+    static getById = async (req: Request, res: Response) => {
+        console.log('oliwis id getbyid get')
         const {id} = req.params
         console.log(id)
     }
 
-    static updateById = async  (req: Request, res: Response) => {
-        console.log('oliwis id put')
+    static updateById = async (req: Request, res: Response) => {
+        console.log('oliwis id updatebyid put')
         const {id} = req.params
         console.log(id)
     }
 
-    static deleteById = async  (req: Request, res: Response) => {
-        console.log('oliwis id delete')
+    static deleteById = async (req: Request, res: Response) => {
+        console.log('oliwis id deletebyid delete')
         const {id} = req.params
         console.log(id)
     }
