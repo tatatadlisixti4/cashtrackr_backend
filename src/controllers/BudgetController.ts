416fcd831c285ba4ff8a50dc5458185598fe4584
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-28 15:35:34 -0300

    Mostrando los gastos que pertenecen a un presupuesto

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,5 +1,6 @@
 import type {Request, Response} from 'express'
 import Budget from '../models/Budget'
+import Expense from '../models/Expense'
 
 export class BudgetController {
     static getAll = async (req: Request, res: Response) => {
@@ -27,7 +28,8 @@ export class BudgetController {
     }
 
     static getById = async (req: Request, res: Response) => {
-        res.json(req.budget)
+        const budget = await Budget.findByPk(req.budget.id, {include: [Expense]})
+        res.json(budget)
     }
 
     static updateById = async (req: Request, res: Response) => {
