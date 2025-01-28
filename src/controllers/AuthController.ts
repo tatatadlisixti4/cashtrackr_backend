ba982f58c009101a59e8db4e5261b5cc9b5e149c
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-28 17:21:03 -0300

    Registrando usuarios

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,14 @@
 import type {Request, Response} from 'express'
+import User from '../models/User'
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
-        res.json('asdasdsadsadas')
+        try {
+            const user = new User(req.body)
+            await user.save()
+            res.json('Usuario creado con éxito')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
+        }
     }
 }
