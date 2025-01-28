6bda111fabeb26494a4aa77e3119113100790a30
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-28 17:32:47 -0300

    Revisar que un usuario exista antes de registrarlo

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,6 +3,15 @@ import User from '../models/User'
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
+        const {email} = req.body
+        const userExist = await User.findOne({where: {email}})
+        
+        if(userExist) {
+            const error = new Error('El usuario ya existe')
+            res.status(409).json({error: error.message})
+            return
+        }
+
         try {
             const user = new User(req.body)
             await user.save()
@@ -10,5 +19,6 @@ export class AuthController {
         } catch (error) {
             res.status(500).json({error: 'Hubo un error'})
         }
+
     }
 }
