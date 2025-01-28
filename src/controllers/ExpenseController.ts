90576960e7e47916b15c69818d267b78a223e2b1
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-27 21:13:03 -0300

    Rutas y métodos del controlador

---
+++ src/controllers/ExpenseController.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from 'express'
+
+export class ExpensesController {
+    static getAll = async (req: Request, res: Response) => {
+    
+    }
+
+    static create = async (req: Request, res: Response) => {
+    
+    }
+
+    static getById = async (req: Request, res: Response) => {
+    
+    }
+
+    static updateById = async (req: Request, res: Response) => {
+    
+    }
+
+    static deleteById = async (req: Request, res: Response) => {
+    
+    }
+}
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,30 +1,34 @@
 import {Router} from 'express'
 import {body, param} from 'express-validator'
 import {BudgetController} from '../controllers/BudgetController'
+import {ExpensesController} from '../controllers/ExpenseController'
 import {handleInputErrors} from '../middleware/validation'
 import {validateBudgetExists, validateBudgetId, validateBudgetInput} from '../middleware/budget'
 
 const router = Router()
 
+/** Routes for budget */
 router.param('budgetId', validateBudgetId)
 router.param('budgetId', validateBudgetExists)
-
 router.get('/', BudgetController.getAll)
-
 router.post('/', 
     validateBudgetInput,
     handleInputErrors,
     BudgetController.create
 )
-
 router.get('/:budgetId', BudgetController.getById)
-
 router.put('/:budgetId',
     validateBudgetInput,  
     handleInputErrors,
     BudgetController.updateById
 )
-
 router.delete('/:budgetId', BudgetController.deleteById)
+
+/** Router for expenses */
+router.get('/:budgetId/expenses', ExpensesController.getAll)
+router.post('/:budgetId/expenses', ExpensesController.create)
+router.get('/:budgetId/expenses/:expenseId', ExpensesController.getById)
+router.put('/:budgetId/expenses/:expenseId', ExpensesController.updateById)
+router.delete('/:budgetId/expenses/:expenseId', ExpensesController.deleteById)
 
 export default router
