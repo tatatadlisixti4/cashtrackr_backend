3c6f02d101a7bc73908c32e87b4a427eb01c0c01
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-10 19:29:18 -0300

    beforeEach al testing

--- src/tests/unit/BudgetController.test.ts
+++ src/tests/unit/BudgetController.test.ts
@@ -8,17 +8,32 @@ jest.mock('../../models/Budget', () => ({
 }))
 
 describe('BudgetController.getAll', () => {
+    beforeEach(() => {
+        (Budget.findAll as jest.Mock).mockReset()
+        ;(Budget.findAll as jest.Mock).mockImplementation(options => {
+            const updatedBudgets = budgets.filter(budget => budget.userId === options.where.userId)
+            return Promise.resolve(updatedBudgets)
+        })
+    })
     it('should retrive 2 budgets for user with ID 1', async () => {
         const req = createRequest({
             user: {id: 1}
         })
         const res = createResponse()
-        const updatedBudgets = budgets.filter(budget =>  budget.userId === req.user.id)
-        ;(Budget.findAll as jest.Mock).mockResolvedValue(updatedBudgets)
-
         await BudgetController.getAll(req, res)
         const data = res._getJSONData()
         expect(data).toHaveLength(2)
+        expect(res.statusCode).toBe(200)
+        expect(res.statusCode).not.toBe(404)
+    })
+    it('should retrive 1 budget for user with ID 2', async () => {
+        const req = createRequest({
+            user: {id: 2}
+        })
+        const res = createResponse()
+        await BudgetController.getAll(req, res)
+        const data = res._getJSONData()
+        expect(data).toHaveLength(1)
         expect(res.statusCode).toBe(200)
         expect(res.statusCode).not.toBe(404)
     })
