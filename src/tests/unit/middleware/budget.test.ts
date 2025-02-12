7afbef92bb3dd388a31ca04d0e8c32cce02dc7e0
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-11 21:21:04 -0300

    Testing middleware que valida si un presupuesto existe

--- src/tests/unit/middleware/budget.test.ts
+++ src/tests/unit/middleware/budget.test.ts
@@ -1,6 +1,7 @@
 import {createRequest, createResponse} from 'node-mocks-http'
 import {validateBudgetExists} from '../../../middleware/budget'
 import Budget from '../../../models/Budget'
+import { budgets } from '../../mocks/budgets'
 
 jest.mock('../../../models/Budget', () => ({
     findByPk: jest.fn()
@@ -22,5 +23,20 @@ describe('budget - validateBudgetExists', () => {
         expect(res.statusCode).toBe(404)
         expect(data).toEqual({error: 'Presupuesto no existe'})
         expect(next).not.toHaveBeenCalled()
+    })
+
+    it('should procees to next middleware if budget exists', async () => {
+        (Budget.findByPk as jest.Mock).mockResolvedValue(budgets[0])
+        const req = createRequest({
+            params: {
+                budgetId: 1
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+        await validateBudgetExists(req, res, next)
+        expect(res.statusCode).toBe(200)
+        expect(req.budget).toEqual(budgets[0])
+        expect(next).toHaveBeenCalled()
     })
 })
