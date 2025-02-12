623a97fcf1aa43ed41b8bbe8b0d413c51ba377fb
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-12 15:07:25 -0300

    Testing a actualizar y eliminar por id

--- src/tests/unit/controllers/ExpenseController.test.ts
+++ src/tests/unit/controllers/ExpenseController.test.ts
@@ -60,3 +60,47 @@ describe('ExpenseController.getById', () => {
         expect(data).toEqual(expenses[0])
     })
 })
+
+describe('ExpenseController.updateById', () => {
+    it('should update expense with ID 1', async () => {
+        const mockExpense = {
+            ...expenses[0],
+            update: jest.fn().mockResolvedValue(true)
+        }
+        const req = createRequest({
+            method: 'PUT',
+            url: '/api/budgets/:budgetId/expenses/:expenseId',
+            expense: mockExpense,
+            body: {name: 'Updated Expense', amount: 500},
+        })
+        const res = createResponse()
+        await ExpensesController.updateById(req, res)
+        const data = res._getJSONData()
+        expect(res.statusCode).toBe(200)
+        expect(data).toBe('Gasto actualizado correctamente')
+        expect(mockExpense.update).toHaveBeenCalled()
+        expect(mockExpense.update).toHaveBeenCalledTimes(1)
+        expect(mockExpense.update).toHaveBeenCalledWith(req.body)
+    })
+})
+
+describe('ExpenseController.deleteById', () => {
+    it('should delete expense with ID 1', async () => {
+        const mockExpense = {
+            ...expenses[0],
+            destroy: jest.fn().mockResolvedValue(true)
+        }
+        const req = createRequest({
+            method: 'DELETE',
+            url: '/api/budgets/:budgetId/expenses/:expenseId',
+            expense: mockExpense
+        })
+        const res = createResponse()
+        await ExpensesController.deleteById(req, res)
+        const data = res._getJSONData()
+        expect(res.statusCode).toBe(200)
+        expect(data).toBe('Gasto eliminado correctamente')
+        expect(mockExpense.destroy).toHaveBeenCalled()
+        expect(mockExpense.destroy).toHaveBeenCalledTimes(1)
+    })
+})
