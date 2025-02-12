88b73e27639046fb2514c19d29dc23ae1a8d5754
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-12 17:21:03 -0300

    Testing para crear un usuario

--- src/tests/unit/controllers/AuthController.test.ts
+++ src/tests/unit/controllers/AuthController.test.ts
@@ -1,8 +1,12 @@
 import {createRequest, createResponse} from "node-mocks-http"
 import {AuthController} from "../../../controllers/AuthController"
 import User from "../../../models/User"
+import {hashPassword} from "../../../utils/auth"
+import {generateToken} from "../../../utils/token"
 
 jest.mock('../../../models/User')
+jest.mock('../../../utils/auth')
+jest.mock('../../../utils/token')
 
 describe('AuthController.createAccount', () => {
     it('should return a 409 status and an error message if the email is already  registered', async () => {
@@ -23,6 +27,28 @@ describe('AuthController.createAccount', () => {
         expect(User.findOne).toHaveBeenCalled()
         expect(User.findOne).toHaveBeenCalledTimes(1)
         expect(User.findOne).toHaveBeenCalledWith({where: {email: req.body.email}})
-        expect(User.create).not.toHaveBeenCalled()
     })
+
+    it('should register a new user and return a success message', async () => {
+        (hashPassword as jest.Mock).mockResolvedValue('123456789');
+        (generateToken as jest.Mock).mockReturnValue('123456');
+        const req = createRequest({
+            method: 'POST',
+            url: '/api/auth/create-account', 
+            body: {
+                email: 'test@test.com',
+                password: '123456789'
+            }
+        })
+        const res = createResponse()
+        const mockUser = {
+            ...req.body,
+            password: await hashPassword(req.body.password),
+            token: generateToken()
+        };
+        (User.findOne as jest.Mock).mockResolvedValue(null);
+        (User.create as jest.Mock).mockResolvedValue(mockUser);
+        await AuthController.createAccount(req, res)
+    })
+        
 })
