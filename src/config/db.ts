2ab90ef305e943fc9f57a7854f481665cba13d2e
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-14 01:39:16 -0300

    Conexión a bd local en entorno de desarrollo o a render en producción

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,12 +2,38 @@ import {Sequelize} from 'sequelize-typescript'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const db = new Sequelize(process.env.DATABASE_URL, {
-    models: [__dirname + '/../models/**/*'],
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: false
+let db: Sequelize
+
+if(process.env.NODE_ENV === 'production') {
+    db = new Sequelize(process.env.DATABASE_URL, {
+        models: [__dirname + '/../models/**/*'],
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: false
+            }
         }
-    }
-})
+    })
+} else {
+    console.log('a');
+    
+    db = new Sequelize({
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        models: [__dirname + '/../models/**/*'],
+        logging: false,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
+}
+
+export default db
+
