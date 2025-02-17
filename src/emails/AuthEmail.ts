5e46a0dc36932e8b660ec388ab004f500a6accd3
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-17 17:36:28 -0300

    Ajustando el email para que lleva al uisuario a confirmar cuenta

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -8,29 +8,29 @@ type EmailType = {
 
 export class AuthEmail  {
     static sendConfirmationEmail = async (user: EmailType) => {
-        // const email = await transport.sendMail({
-        //     from: 'CashTrackr <admin@cashtrackr.com>',
-        //     to: user.email,
-        //     subject: 'CashTrackr - Confirma tu cuenta',
-        //     html: `
-        //         <p>Hola: ${user.name}, has creado tu cuenta en CashTrackr, ya está caso lista.</p>
-        //         <p>Visita el siguiente enlace:</p>
-        //         <a href="#">Confirmar cuenta<a/>
-        //         <p>Ingresa el código: <b>${user.token}</b></p>`
-        // })
-        // console.log('Mensaje enviado', email.messageId)
+        const email = await transport.sendMail({
+            from: 'CashTrackr <admin@cashtrackr.com>',
+            to: user.email,
+            subject: 'CashTrackr - Confirma tu cuenta',
+            html: `
+                <p>Hola: ${user.name}, has creado tu cuenta en CashTrackr, ya está caso lista.</p>
+                <p>Visita el siguiente enlace:</p>
+                <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta<a/>
+                <p>Ingresa el código: <b>${user.token}</b></p>`
+        })
+        console.log('Mensaje enviado', email.messageId)
     } 
     static sendPasswordResetToken = async (user: EmailType) => {
-        // const email = await transport.sendMail({
-        //     from: 'CashTrackr <admin@cashtrackr.com>',
-        //     to: user.email,
-        //     subject: 'CashTrackr - Reestablece tu password',
-        //     html: `
-        //         <p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
-        //         <p>Visita el siguiente enlace:</p>
-        //         <a href="#">Reestablecer password<a/>
-        //         <p>Ingresa el código: <b>${user.token}</b></p>`
-        // })
-        // console.log('Mensaje enviado', email.messageId)
+        const email = await transport.sendMail({
+            from: 'CashTrackr <admin@cashtrackr.com>',
+            to: user.email,
+            subject: 'CashTrackr - Reestablece tu password',
+            html: `
+                <p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
+                <p>Visita el siguiente enlace:</p>
+                <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer password<a/>
+                <p>Ingresa el código: <b>${user.token}</b></p>`
+        })
+        console.log('Mensaje enviado', email.messageId)
     } 
 }
